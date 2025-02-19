31515ed29ed959bec3d798a6cfa8adba2b7565f6
wanghoc <trieuquanghocts@gmail.com>
2025-02-19 22:45:26 +0700

    update home.homedart, add map

--- lib/home.dart
+++ lib/home.dart
@@ -1,105 +1,141 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Location.dart';
 import 'package:flutter_application_1/account.dart';
 import 'package:flutter_application_1/information.dart';
 import 'package:flutter_application_1/models/category_model.dart';
 import 'package:flutter_application_1/models/map_model.dart';
-import 'package:flutter/widgets.dart'; // Import thêm package nếu cần để sử dụng Navigator
+import 'package:photo_view/photo_view.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-   TextEditingController searchPlaceController = TextEditingController();
+  TextEditingController searchPlaceController = TextEditingController();
   int currentPageIndex = 0;
   List<CategoryModel> categories = [];
-  List<MapModel> maps=[];
+  List<MapModel> maps = [];
+  bool _isDialogDismissed = false; // Kiểm soát việc tắt hộp thoại
+
+  @override
+  void initState() {
+    super.initState();
+    getCategories();
+    getMaps();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _showGuideDialog());
+  }
+
   void getCategories() {
     categories = CategoryModel.getCategories();
   }
+
   void getMaps() {
     maps = MapModel.getMaps();
   }
 
+  void _showGuideDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // Ngăn chặn đóng hộp thoại khi nhấn ngoài
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Vuốt để di chuyển", style: TextStyle(fontSize: 18)),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.chevron_left, size: 30),
+                  Icon(Icons.swipe, size: 50),
+                  Icon(Icons.chevron_right, size: 30),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isDialogDismissed = true;
+                  });
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text("OK", style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    getCategories();
-    getMaps();
     return Scaffold(
       backgroundColor: Color(0xffFFEBCD),
       bottomNavigationBar: _bottomNavBar(),
       body: Column(
         children: [
           _searchField(),
-          SizedBox(height: 25),
+          SizedBox(height: 20),
           _categoriesMethod(),
-           SizedBox(height: 25),
-          
-          Container(
-                width: 378,
-              height: 600,
-              decoration: BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: BorderRadius.circular(25),
-            ),
-            
-            ),
+          SizedBox(height: 20),
+          _isDialogDismissed ? _buildMapSection() : Container(),
         ],
       ),
     );
   }
-NavigationBar _bottomNavBar() {
-  return NavigationBar(
-    onDestinationSelected: (int index) {
-      setState(() {
-        currentPageIndex = 0; // Cập nhật currentPageIndex khi chọn mục mới
-      });
 
-      // Điều hướng đến các trang tương ứng khi chọn icon
-      if (index == 0) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-        );
-      } else if (index == 1) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => InformationPage()),
-        );
-      } else if (index == 2) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => AccountPage()),
-        );
-      }
-    },
-    indicatorColor: Colors.amber,
-    selectedIndex: currentPageIndex, // Cập nhật trạng thái selected icon
-    destinations: const <Widget>[
-      NavigationDestination(
-        selectedIcon: Icon(Icons.home),
-        icon: Icon(Icons.home_outlined),
-        label: 'Trang chủ',
+  Widget _searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+          )
+        ],
       ),
-      NavigationDestination(
-        icon: Badge(child: Icon(Icons.book_online_outlined)),
-        label: 'Thông tin',
-      ),
-      NavigationDestination(
-        icon: Badge(
-          child: Icon(Icons.manage_accounts_outlined),
+      child: TextFormField(
+        controller: searchPlaceController,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Tìm kiếm địa điểm ...',
+          hintStyle: TextStyle(color: Colors.green, fontSize: 18),
+          prefixIcon: Icon(Icons.gps_fixed, size: 25, color: Colors.black),
+          suffixIcon: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: Icon(Icons.mic, color: Colors.black),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search, color: Colors.black),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-        label: 'Tài khoản',
       ),
-    ],
-  );
-}
+    );
+  }
 
-  Container _categoriesMethod() {
+  Widget _categoriesMethod() {
     return Container(
       height: 40,
       child: ListView.separated(
@@ -109,24 +145,20 @@ NavigationBar _bottomNavBar() {
         separatorBuilder: (context, index) => SizedBox(width: 10),
         itemBuilder: (context, index) {
           return Container(
-            width:180,
+            width: 120,
             decoration: BoxDecoration(
-                color: categories[index].boxColor,
-                borderRadius: BorderRadius.circular(16)),
+              color: categories[index].boxColor,
+              borderRadius: BorderRadius.circular(16),
+            ),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: categories[index].icons,
-                  ),
-                ),
+                categories[index].icons,
+                SizedBox(width: 5),
                 Text(
                   categories[index].name,
                   style: TextStyle(
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.bold,
                       color: Colors.black,
                       fontSize: 14),
                 ),
@@ -138,60 +170,60 @@ NavigationBar _bottomNavBar() {
     );
   }
 
-  Container _searchField() {
-    return Container(
-      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-              color: Color(0xff1D1617).withOpacity(0.11),
-              blurRadius: 10,
-              spreadRadius: 0.0)
-        ],
-      ),
-      child: TextFormField(
-        controller: searchPlaceController,
-        onTap: (){
-           Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => LocationPage()),
-        );
-        },
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: 'Search',
-          hintStyle: TextStyle(
-            color: Colors.green,
-            fontSize: 20
+  Widget _buildMapSection() {
+    return Expanded(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: PhotoView(
+          imageProvider: AssetImage("../assets/icon/map_test.jpg"),
+          minScale: PhotoViewComputedScale.contained,
+          maxScale: PhotoViewComputedScale.covered * 2,
+          enableRotation: true,
+          backgroundDecoration: BoxDecoration(
+            color: Colors.white,
           ),
-          
-
-          prefixIcon: Icon(
-           Icons.gps_fixed,
-           size: 25
-            ),
-          
-          suffixIcon: IntrinsicHeight(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                VerticalDivider(
-                  color: Colors.black,
-                  indent: 10,
-                  endIndent: 10,
-                  thickness: 0.1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.search)
-                ),
-              ],
-            ),
-          ),
-          
         ),
       ),
+    );
+  }
+
+  NavigationBar _bottomNavBar() {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        if (index != currentPageIndex) {
+          setState(() {
+            currentPageIndex = index;
+          });
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => InformationPage()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AccountPage()),
+            );
+          }
+        }
+      },
+      indicatorColor: Colors.amber,
+      selectedIndex: currentPageIndex,
+      destinations: [
+        NavigationDestination(
+          selectedIcon: Icon(Icons.home),
+          icon: Icon(Icons.home_outlined),
+          label: 'Trang chủ',
+        ),
+        NavigationDestination(
+          icon: Badge(child: Icon(Icons.book_online_outlined)),
+          label: 'Thông tin',
+        ),
+        NavigationDestination(
+          icon: Badge(child: Icon(Icons.manage_accounts_outlined)),
+          label: 'Tài khoản',
+        ),
+      ],
     );
   }
 }
