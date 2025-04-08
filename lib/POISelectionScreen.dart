9d924b3ba614aff3528ae4c544f37579220dc49f
NgocSongNe <148341689+NgocSongNe@users.noreply.github.com>
2025-04-08 22:11:29 +0700

    [Bao] - Update the logic for the model api - Done

--- lib/POISelectionScreen.dart
+++ lib/POISelectionScreen.dart
@@ -121,7 +121,7 @@ class POISelectionScreen {
     for (String endpoint in geoJsonEndpoints) {
       try {
         final response =
-            await http.get(Uri.parse("http://10.0.2.2:5000$endpoint"));
+            await http.get(Uri.parse("http://192.168.1.6:8765$endpoint"));
         if (response.statusCode == 200) {
           final geoJson = jsonDecode(response.body);
           if (geoJson['features'] is List) {
@@ -221,7 +221,7 @@ class POISelectionScreen {
   Future<void> _loadWallsFromAPI() async {
     try {
       final response =
-          await http.get(Uri.parse("http://10.0.2.2:5000/geojson/Paths"));
+          await http.get(Uri.parse("http://192.168.1.6:8765/geojson/Paths"));
       if (response.statusCode == 200) {
         final pathsJson = json.decode(response.body);
         walls = (pathsJson['features'] as List).map<List<LatLng>>((feature) {
@@ -241,7 +241,7 @@ class POISelectionScreen {
   Future<void> _loadPOIData() async {
     try {
       final response =
-          await http.get(Uri.parse("http://10.0.2.2:5000/geojson/POI"));
+          await http.get(Uri.parse("http://192.168.1.6:8765/geojson/POI"));
       if (response.statusCode == 200) {
         final poiJson = json.decode(response.body);
 
